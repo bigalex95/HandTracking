3b8c863cb59e5f4bdc730fb2fb014d774d9d0dba
Alibek <alibek060395@gmail.com>
2021-02-04 11:31:49 +0300

    configured liveDemov2

---
+++ live_demoV2.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+import json
+import trt_pose.coco
+import trt_pose.models
+import torch
+import torch2trt
+from torch2trt import TRTModule
+import time
+import cv2
+import torchvision.transforms as transforms
+import PIL.Image
+import numpy as np
+from trt_pose.draw_objects import DrawObjects
+from trt_pose.parse_objects import ParseObjects
+from imutils.video import FPS
+from threading import Thread
+
+with open('human_pose.json', 'r') as f:
+    human_pose = json.load(f)
+
+topology = trt_pose.coco.coco_category_to_topology(human_pose)
+
+num_parts = len(human_pose['keypoints'])
+num_links = len(human_pose['skeleton'])
+
+model = trt_pose.models.resnet18_baseline_att(num_parts, 2 * num_links).cuda().eval()
+
+MODEL_WEIGHTS = 'resnet18_baseline_att_224x224_A_epoch_249.pth'
+
+model.load_state_dict(torch.load(MODEL_WEIGHTS))
+
+WIDTH = 224
+HEIGHT = 224
+
+data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+
+model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+
+OPTIMIZED_MODEL = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
+
+torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
+
+model_trt = TRTModule()
+model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
+
+t0 = time.time()
+torch.cuda.current_stream().synchronize()
+for i in range(50):
+    y = model_trt(data)
+torch.cuda.current_stream().synchronize()
+t1 = time.time()
+
+print(50.0 / (t1 - t0))
+
+mean = torch.Tensor([0.485, 0.456, 0.406]).cuda()
+std = torch.Tensor([0.229, 0.224, 0.225]).cuda()
+device = torch.device('cuda')
+
+def preprocess(image):
+    global device
+    device = torch.device('cuda')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = PIL.Image.fromarray((image * 255).astype(np.uint8))
+    #image = PIL.Image.fromarray(image).astype(np.uint8))
+    image = transforms.functional.to_tensor(image).to(device)
+    image.sub_(mean[:, None, None]).div_(std[:, None, None])
+    return image[None, ...]
+
+parse_objects = ParseObjects(topology)
+draw_objects = DrawObjects(topology)
+
+class WebcamVideoStream:
+    def __init__(self, src=0, device=None):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src, device)
+        (self.grabbed, self.frame) = self.stream.read()
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+    	    # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+	    
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=3840,
+    capture_height=2160,
+    display_width=1920, #2560
+    display_height=1080, #1440
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def execute(change):
+    image = change['new']
+    data = preprocess(image)
+    cmap, paf = model_trt(data)
+    cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
+    counts, objects, peaks = parse_objects(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
+    draw_objects(image, counts, objects, peaks)
+    cv2.imshow("execute", image)
+def main():
+    vs1 = WebcamVideoStream(src=gstreamer_pipeline(sensor_id=0), device=cv2.CAP_GSTREAMER).start()
+    cap = cv2.VideoCapture(4)
+    while True:
+        #frame1 = vs1.read()
+        _, frame1 = cap.read()
+        #img = cv2.resize(frame1, (224,224))
+        frame1 = frame1[...,::-1]
+        image_tf = tf.convert_to_tensor(frame1)
+        print(image_tf.shape)
+        resize_tf = tf.image.resize(image_tf, (224, 224))
+        cv2.imshow("frame1", frame1)
+        #execute({'new': image_tf.numpy()})
+
+    cv2.destroyAllWindows()
+    vs1.stop()
