20e04c918f7835ba3ef57841534b652ecde8b9ab
Alibek <alibek060395@gmail.com>
2021-02-04 11:35:36 +0300

    added gpu limiting

--- live_demoV2.py
+++ live_demoV2.py
@@ -15,6 +15,17 @@ from trt_pose.parse_objects import ParseObjects
 from imutils.video import FPS
 from threading import Thread
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+
 with open('human_pose.json', 'r') as f:
     human_pose = json.load(f)
 
