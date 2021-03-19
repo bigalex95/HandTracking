6c43f1027cbc14427af3a66c6a04d9b832066da8
Alibek <alibek060395@gmail.com>
2021-03-19 13:06:40 +0300

    added 2 virtual webcam

--- webcamServer.py
+++ webcamServer.py
@@ -48,13 +48,14 @@ def main():
     server1 = imagiz.TCP_Server(port=5550)
     server1.start()
 
-    cv2.namedWindow("frame1", cv2.WINDOW_NORMAL)
-    cv2.namedWindow("frame2", cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("frame1", cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("frame2", cv2.WINDOW_NORMAL)
 
     frame1 = np.zeros((256, 256, 3))
     frame2 = np.zeros((256, 256, 3))
 
-    camera = pyfakewebcam.FakeWebcam('/dev/video1', 640, 480)
+    camera1 = pyfakewebcam.FakeWebcam('/dev/video0', 256, 256)
+    camera2 = pyfakewebcam.FakeWebcam('/dev/video1', 256, 256)
 
     fps = FPS().start()
     while True:
@@ -75,7 +76,8 @@ def main():
                 fps.update()
             # cv2.imshow("frame1", frame1)
             # cv2.imshow("frame2", frame2)
-            camera.schedule_frame(frame1)
+            camera1.schedule_frame(frame1)
+            camera2.schedule_frame(frame2)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             fps.update()
